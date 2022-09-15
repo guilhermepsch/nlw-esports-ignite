aca0b8cff837ca7e3bd24bc8f522785f7638a0ee
Schneidt <guilhermepsch@gmail.com>
2022-09-15 14:18:16 -0300

    chore: checkpoint

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,13 +1,37 @@
-import express from 'express'
+import express from "express";
 
-const app = express()
+const app = express();
 
-app.get('/ads', (request, response) => {
-  return response.json([
-    { id: 1, name: 'Anúncio 1'},
-    { id: 2, name: 'Anúncio 2'},
-    { id: 3, name: 'Anúncio 3'},
-  ])
-})
+app.get("/games", (req, res) => {
+  return res.json([]);
+});
 
-app.listen(3333) 
+app.post("/ads", (req, res) => {
+  return res.status(201).json([]);
+});
+
+app.get("/games/:id/ads", (req, res) => {
+  const gameId = req.params.id;
+
+  return res.json([
+    { id: 1, name: "Anúncio 1" },
+    { id: 2, name: "Anúncio 2" },
+    { id: 3, name: "Anúncio 3" },
+  ]);
+});
+
+app.get("/ads", (req, res) => {
+  return res.json([
+    { id: 1, name: "Anúncio 1" },
+    { id: 2, name: "Anúncio 2" },
+    { id: 3, name: "Anúncio 3" },
+  ]);
+});
+
+app.get("/ads/:id/discord", (req, res) => {
+  const adId = req.params.id;
+
+  return res.json([]);
+});
+
+app.listen(3333);
